de4382f208237b62f7bac97ab3adfe1d63d3d216
Silvio Angelo <silvioangels@gmail.com>
2015-06-30 11:48:53 -0300

    Atualizacao com o ciclo de vida dos Beans

--- TutorialsPoint_SpringBeanLifeCycle/src/com/tutorialspoint/MainApp.java
+++ TutorialsPoint_SpringBeanLifeCycle/src/com/tutorialspoint/MainApp.java
@@ -1,28 +1,35 @@
 package com.tutorialspoint;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
 
 	public static void main(String[] args) {
 
-		beanScopeSingleton();
-
-	}
-
-	private static void beanScopeSingleton() {
-		System.out.println("#######################################################");
-		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-
-		HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
-
-		objA.setMessage("I'm object A");
-		objA.getMessage();
-
-		HelloWorld objB = (HelloWorld) context.getBean("helloWorld");
-		objB.getMessage();
+		initDestroyBeanConfig();
+		initDestroyDefaultAllBean();
 
 	}
 	
+	private static void initDestroyBeanConfig(){
+		System.out.println("###########################################################");
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+
+		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+		obj.getMessage();
+		context.registerShutdownHook();
+		
+	}
+	
+	private static void initDestroyDefaultAllBean(){
+		System.out.println("###########################################################");
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("BeansDefault.xml");
+
+		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+		obj.getMessage();
+		context.registerShutdownHook();
+		
+	}
+
 }
