5b74c83c4571f326d5d670f79fe67cb55a79a5e8
Silvio Angelo <silvioangels@gmail.com>
2015-06-30 12:35:01 -0300

    Adicionando Classes com Construtor Padrao diferente

---
+++ TutorialsPoint_DependencyInjection/src/x/y/Foo.java
@@ -0,0 +1,11 @@
+package x.y;
+
+public class Foo {
+	
+	public Foo(int year, String name) {
+
+		System.out.println("Inside Foo Constructor: "+"year: "+year+" name: "+name);
+	
+	}
+
+}
---
+++ TutorialsPoint_DependencyInjection/src/x/y/FooIndex.java
@@ -0,0 +1,11 @@
+package x.y;
+
+public class FooIndex {
+	
+	public FooIndex(int year, String name) {
+
+		System.out.println("Inside FooIndex Constructor: "+"year: "+year+" name: "+name);
+	
+	}
+
+}
