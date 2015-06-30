5cec291bd95652da79460dd4ce2190dbfaa1dc33
Silvio Angelo <silvioangels@gmail.com>
2015-06-30 16:11:59 -0300

    Adding Spring Annotation Based Configuration Project

---
+++ TutorialsPoint_AnnotationBasedConfiguration/src/com/tutorialspoint/annotationbasedconfiguration/required/Student.java
@@ -0,0 +1,28 @@
+package com.tutorialspoint.annotationbasedconfiguration.required;
+
+import org.springframework.beans.factory.annotation.Required;
+
+public class Student {
+
+	private Integer age;
+	private String name;
+
+	@Required
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	@Required
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
