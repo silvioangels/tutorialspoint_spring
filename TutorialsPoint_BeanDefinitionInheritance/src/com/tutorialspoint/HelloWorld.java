cd22c243e389153d3c768246a65268ed5b72fede
Silvio Angelo <silvioangels@gmail.com>
2015-06-30 12:14:10 -0300

    Alterações de definição das Classes de Herança

--- TutorialsPoint_BeanDefinitionInheritance/src/com/tutorialspoint/HelloWorld.java
+++ TutorialsPoint_BeanDefinitionInheritance/src/com/tutorialspoint/HelloWorld.java
@@ -2,22 +2,23 @@ package com.tutorialspoint;
 
 public class HelloWorld {
 
-	private String message;
+	private String message1;
+	private String message2;
 
-	public void setMessage(String message) {
-		this.message = message;
+	public void setMessage1(String message) {
+		this.message1 = message;
 	}
 
-	public void getMessage() {
-		System.out.println("Your Message : " + message);
+	public void setMessage2(String message) {
+		this.message2 = message;
 	}
 
-	public void init() {
-		System.out.println("Bean is going through init.");
+	public void getMessage1() {
+		System.out.println("World Message1 : " + message1);
 	}
 
-	public void destroy() {
-		System.out.println("Bean will destroy now.");
+	public void getMessage2() {
+		System.out.println("World Message2 : " + message2);
 	}
 
 }
