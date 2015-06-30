0ddc30afa4264165746a6c982c7c20f02de92aca
Silvio Angelo <silvioangels@gmail.com>
2015-06-30 11:59:50 -0300

    Atualizacao das mensagens

--- TutorialsPoint_BeanPostProcessor/src/com/tutorialspoint/InitHelloWorld.java
+++ TutorialsPoint_BeanPostProcessor/src/com/tutorialspoint/InitHelloWorld.java
@@ -4,17 +4,18 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 
 public class InitHelloWorld implements BeanPostProcessor {
-
+	
 	@Override
-	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
 		System.out.println("BeforeInitialization : " + beanName);
 		return bean; // you can return any other object as well
 	}
 
 	@Override
-	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
 		System.out.println("AfterInitialization : " + beanName);
 		return bean; // you can return any other object as well
 	}
+
 
 }
