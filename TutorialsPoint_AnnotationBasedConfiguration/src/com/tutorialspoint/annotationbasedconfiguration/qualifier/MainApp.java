c558d70f85d05026eec48d2e72b79221352f4aa9
Silvio Angelo <silvioangels@gmail.com>
2015-06-30 16:40:52 -0300

    adding qualifier examples

---
+++ TutorialsPoint_AnnotationBasedConfiguration/src/com/tutorialspoint/annotationbasedconfiguration/qualifier/MainApp.java
@@ -0,0 +1,17 @@
+package com.tutorialspoint.annotationbasedconfiguration.qualifier;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+
+		Profile profile = (Profile) context.getBean("profile");
+
+		profile.printAge();
+		profile.printName();
+	}
+
+}
