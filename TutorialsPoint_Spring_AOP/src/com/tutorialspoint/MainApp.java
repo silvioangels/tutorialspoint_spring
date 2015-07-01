84f9632457051d8d7328b875c23a9c669600b139
Silvio Angelo <silvioangels@gmail.com>
2015-07-01 10:34:32 -0300

    Adding AOP Classes

---
+++ TutorialsPoint_Spring_AOP/src/com/tutorialspoint/MainApp.java
@@ -0,0 +1,27 @@
+package com.tutorialspoint;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("BeansStudentAdvice.xml");
+
+		Student student = (Student) context.getBean("student");
+
+		student.getName();
+		student.getAge();
+
+		context = new ClassPathXmlApplicationContext("Beans.xml");
+
+		student = (Student) context.getBean("student");
+
+		student.getName();
+		student.getAge();
+
+		student.printThrowException();
+
+	}
+
+}
