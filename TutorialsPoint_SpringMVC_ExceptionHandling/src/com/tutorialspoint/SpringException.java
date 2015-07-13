15fddfa1c83c70291eb6dfee325a80348d99ed47
Silvio Angelo <silvioangels@gmail.com>
2015-07-13 15:26:14 -0300

    Adding Spring Project Exception Handling Example

---
+++ TutorialsPoint_SpringMVC_ExceptionHandling/src/com/tutorialspoint/SpringException.java
@@ -0,0 +1,19 @@
+package com.tutorialspoint;
+
+public class SpringException extends RuntimeException {
+
+	private String exceptionMsg;
+
+	public SpringException(String exceptionMsg) {
+		this.exceptionMsg = exceptionMsg;
+	}
+
+	public String getExceptionMsg() {
+		return this.exceptionMsg;
+	}
+
+	public void setExceptionMsg(String exceptionMsg) {
+		this.exceptionMsg = exceptionMsg;
+	}
+
+}
